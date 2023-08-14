3ff4865049b25a8bd3b892a1ee3e4966a150f36d
VadymKrem <vadimkuzin2@gmail.com>
2023-08-14 20:07:11 +0300

    try2

--- src/pages/MoviesDetails/MoviesDetails.jsx
+++ src/pages/MoviesDetails/MoviesDetails.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import { getMovieDetails } from 'service/getMovies';
 const defaultImg =
   'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
@@ -7,12 +7,19 @@ const defaultImg =
 const MoviesDetails = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+
+  const location = useLocation();
+  const goBackLink = useRef(location.state?.from ?? '/');
+
   useEffect(() => {
     getMovieDetails(movieId).then(setMovie);
   }, [movieId]);
+
   if (!movie) return;
+
   return (
     <>
+      <Link to={goBackLink.current}>Go Back </Link>
       <h2> {movie.title}</h2>
       <img
         src={
